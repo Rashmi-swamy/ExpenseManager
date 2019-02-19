3d38c00f52733b47714dbdc824a685e5e9fb2933
abhishek <abhishek.rana@legatohealth.com>
2019-02-19 14:09:45 +0530

    Update on Group list

--- src/app/west-panel/west-panel.component.ts
+++ src/app/west-panel/west-panel.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./west-panel.component.css']
 })
 export class WestPanelComponent implements OnInit {
-
+listOfGroup=['Group-1','Group-2','Group-3','Group-4'];
   constructor() { }
 
   ngOnInit() {
